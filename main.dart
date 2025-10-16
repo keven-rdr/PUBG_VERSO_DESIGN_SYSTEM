455cae15fff4d640e3d48099f14bbe27df5eddab
KevenRodrigues6868 <kevenlucas2015@gmail.com>
2025-10-16 10:59:22 -0300

    - Ajustando o Main para apresentar novos componentes e inserir animação

--- main.dart
+++ main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'DesignSystem/Samples/actionButtonSampleScreen/action_button_sample_screen.dart';
+import 'DesignSystem/Samples/avatarSampleScreen/avatar_sample_screen.dart';
 import 'DesignSystem/Samples/bottomTabBarSampleScreen/bottom_tab_bar_sample_screen.dart';
 import 'DesignSystem/Samples/cardSampleScreen/card_sample_screen.dart';
+import 'DesignSystem/Samples/comparisonPlatform/comparison_platform_screen.dart';
 import 'DesignSystem/Samples/inputFieldSampleScreen/input_field_sample_screen.dart';
 import 'DesignSystem/Samples/linkedLabelSampleScreen/linked_label_sample_screen.dart';
-import 'DesignSystem/Samples/tabComponentSampleScreen/tab_sample_screen.dart';
-
+import 'DesignSystem/Samples/valueComparisonScreen/value_card_screen.dart';
 
 
 void main() {
@@ -35,7 +37,7 @@ class ButtonScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Design System Sample App'),
+        title: const Text('Design System Sample App - PUBG VERSO'),
       ),
       body: Center(
         child: SizedBox(
@@ -43,11 +45,18 @@ class ButtonScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
+              SizedBox(
+                height: 150,
+                child: Lottie.asset('assets/animations/vs.json'),
+              ),
               _buildButton(context, 'Action Button', ActionButtonPage()),
               _buildButton(context, 'Bottom Tab Bar', BottomTabBarPage()),
               _buildButton(context, 'Input Text Field', InputFieldPage()),
               _buildButton(context, 'Linked Label', LinkedLabelPage()),
               _buildButton(context, 'Card', CardPage()),
+              _buildButton(context, 'Avatar', AvatarPage()),
+              _buildButton(context, 'Platform', PlatformPage()),
+              _buildButton(context, 'Value Card',  ValueCardPage()),
             ],
           ),
         ),
@@ -64,67 +73,6 @@ class ButtonScreen extends StatelessWidget {
         );
       },
       child: Text(text),
-    );
-  }
-}
-
-// Scene Widgets
-class Scene1 extends StatelessWidget {
-  const Scene1({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Scene 1')),
-      body: const Center(child: Text('Welcome to Scene 1')),
-    );
-  }
-}
-
-class Scene2 extends StatelessWidget {
-  const Scene2({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Scene 2')),
-      body: const Center(child: Text('Welcome to Scene 2')),
-    );
-  }
-}
-
-class Scene3 extends StatelessWidget {
-  const Scene3({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Scene 3')),
-      body: const Center(child: Text('Welcome to Scene 3')),
-    );
-  }
-}
-
-class Scene4 extends StatelessWidget {
-  const Scene4({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Scene 4')),
-      body: const Center(child: Text('Welcome to Scene 4')),
-    );
-  }
-}
-
-class Scene5 extends StatelessWidget {
-  const Scene5({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Scene 5')),
-      body: const Center(child: Text('Welcome to Scene 5')),
     );
   }
 }
